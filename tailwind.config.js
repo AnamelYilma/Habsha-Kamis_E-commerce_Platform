881ed82b9bceb290a6e8da2d1bd7326b4b4c9f8b
Anamel Yilma <227770331+AnamelYilma@users.noreply.github.com>
2026-08-08 14:29:38 +0300

    update tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+model.exports = {
+    content: [
+        "./app/**/*.{js,ts ,tsx ,jsx,mdx}",
+        "./componenet/**/*.js,ts ,tsx ,jsx,mdx}"
+    ],
+    them:{
+        extend:{
+            colors:{
+               gold: {
+              light: '#f3e5ab',
+              DEFAULT: '#d4af37',
+              dark: '#aa8010',
+            },
+            ethioGreen: '#078732',
+            ethioYellow: '#FCD116',
+            ethioRed: '#E51F1F',
+            }
+        }
+    },
+    plugins:[],
+}
